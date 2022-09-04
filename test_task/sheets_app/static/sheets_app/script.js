a23ddc67241bb09b2f362b42ee97dbd6e947cf62
VadimMonroe <propoved@mail.ru>
2022-09-04 17:47:56 +0300

    almost done

---
+++ test_task/sheets_app/static/sheets_app/script.js
@@ -0,0 +1,106 @@
+// const id = document.querySelectorAll('#id');
+// const order_number = document.querySelectorAll('#order_number');
+// const cost = document.querySelectorAll('#cost');
+// const deliveryTime = document.querySelectorAll('#delivery_time');
+// const icostRoubles = document.querySelectorAll('#cost_roubles');
+
+// let list_data = [];
+// let labels = [];
+
+// let listCostDate = [['Cost', 'Date'],];
+
+// for (let i = 0; i < id.length; i++) {
+//     list_data.push(Number(cost[i].textContent));
+//     labels.push(deliveryTime[i].textContent);
+//     listCostDate.push([deliveryTime[i].textContent, Number(cost[i].textContent)])
+// }
+
+// function drawChart() {
+//     const data = google.visualization.arrayToDataTable(listCostDate.slice(0, 26));
+
+//     const options = {
+//         chart: {
+//             title: 'Roubles',
+//             subtitle: 'From USD',
+//             curveType: 'none'},
+//         width: 900,
+//         height: 500,
+//         bar: {groupWidth: '95%'},
+//         vAxis: { 
+//             gridlines: { count: 15 }}
+//     };
+
+//     const chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+//     chart.draw(data, options);
+// }
+
+// google.charts.load('current', {'packages':['corechart']});
+// google.charts.setOnLoadCallback(drawChart);
+
+
+
+// -------------
+
+// const id = document.querySelectorAll('#id');
+// const order_number = document.querySelectorAll('#order_number');
+const cost = document.querySelectorAll('#cost');
+const deliveryTime = document.querySelectorAll('#delivery_time');
+const costRoubles = document.querySelectorAll('#cost_roubles');
+const totalPrice = document.querySelector('.totalPriceH1');
+
+let list_cost = [];
+let list_date = [];
+let totalPriceRoubles = 0;
+
+for (let i = 0; i < id.length; i++) {
+    list_cost.push(Number(cost[i].textContent));
+    list_date.push(deliveryTime[i].textContent);
+    totalPriceRoubles += Number(costRoubles[i].textContent);
+}
+
+console.log(totalPriceRoubles)
+totalPrice.text = totalPriceRoubles;
+
+const data = {
+    labels: list_date,
+    datasets: [{
+      label: 'Roubles',
+      backgroundColor: 'white',
+      borderColor: '#3366CC',
+      data: list_cost,
+    }]
+};
+
+const ctx = document.getElementById('myChart').getContext('2d');
+// ctx.canvas.parentNode.style.width= '600px';
+// ctx.canvas.parentNode.style.height = '300px';
+
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: {
+        maintainAspectRatio: true,
+        parsing: {
+            xAxisKey: 'data\\.key',
+            yAxisKey: 'data\\.value'
+        },
+        elements: {
+            line: {
+                tension: 0.2
+            }
+        },
+        datasets: {
+            line: {
+                pointRadius: 2
+            }
+        },
+        scales: {
+            x: {
+                max: 50
+              },
+            y: {
+                stacked: true
+            },
+        }
+    }
+});
--- test_task/sheets_app/views.py
+++ test_task/sheets_app/views.py
@@ -1,25 +1,67 @@
 from django.shortcuts import render
-from .google.main import sheet, sheet_id
-from models import SheetInfo
+# from .google.main import sheet, sheet_id
+from .models import SheetInfo
+# import asyncio
+import threading
+import requests
+from bs4 import BeautifulSoup
+import lxml
+from pprint import pprint
 
-def index(request):
+def write_to_base(data, number):
     django_database = SheetInfo.objects.all()
     
-    try:
-        sheet_base = sheet.values().get(spreadsheetId=sheet_id, range="Лист1").execute()
-    
-        for data in sheet_base['values'][1:]:
-            if django_database.filter(id=data[0]):
-                writeline = django_database.get(id=data[0])
-                writeline.order_number=data[1]
-                writeline.cost=data[2]
-                writeline.delivery_time=data[3]
-                writeline.cost_roubles=0
-                writeline.save()
-            else:
-                django_database.create(id=data[0], order_number=data[1], cost=data[2], delivery_time=data[3], cost_roubles=0)
+    if data:
+        if django_database.filter(id=data[0]):
+            writeline = django_database.get(id=data[0]) if 0 < len(data) else django_database.filter(id=number)
+            if [str(writeline.id), str(writeline.order_number), str(writeline.cost), writeline.delivery_time] != data:
                 
+                writeline.order_number = data[1] if 1 < len(data) else 0
+                writeline.cost = data[2] if 2 < len(data) else 0
+                writeline.delivery_time=data[3] if 3 < len(data) else '0'
+                writeline.cost_roubles = roubles_from_usd(writeline.cost)
+                writeline.save()
+        else:
+            django_database.create(id=data[0], order_number=data[1], cost=data[2], delivery_time=data[3], cost_roubles=roubles_from_usd(data[2]))
+    else:
+        if django_database.filter(id=number):
+            writeline = django_database.get(id=number)
+            writeline.order_number = 0
+            writeline.cost = 0
+            writeline.delivery_time = '0'
+            writeline.cost_roubles = 0
+            writeline.save()
+        else:
+            django_database.create(id=number, order_number=0, cost=0, delivery_time='0', cost_roubles=0)
+    
+
+def index(request):
+    tasks =[]
+    
+    try:
+        # sheet_base = sheet.values().get(spreadsheetId=sheet_id, range="Лист1").execute()
+        # for number, data in enumerate(sheet_base['values'][1:], start=1):
+        sheet_base = ['1', '1249708', '675', '24.05.2022'], ['2', '1182407', '214', '13.05.2022'], ['1', '1249708', '675', '24.05.2022'], ['2', '1182407', '214', '13.05.2022']
+        for number, data in enumerate(sheet_base, start=1):
+
+            # asyncio.run(write_to_base(data, number))
+            task = threading.Thread(target=write_to_base, args=(data, number), daemon=True)
+            task.start()
+            tasks.append(task)
     except Exception as e2:
         print('Cant read from sheet_base in views.py:', e2)
         
-    return render(request, 'sheets_app/index.html')
+    # for i in tasks:
+    #     i.join()
+    
+    
+    
+    django_database = SheetInfo.objects.all()
+    roubles_from_usd(22)
+    return render(request, 'sheets_app/index.html', {'database': django_database})
+
+def roubles_from_usd(usd):
+    response = requests.get('https://www.cbr.ru/scripts/XML_daily.asp')
+    dom = BeautifulSoup(response.text, 'xml')
+    result = usd * float(dom.find(ID='R01235').Value.text.replace(',', '.'))
+    return result
